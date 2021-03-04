e7b1cd68c536fce8738991702c80b1d8099f529e
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-03-04 12:45:36 +0100

    aggiunta bootstrap

--- proj/resources/views/welcome.blade.php
+++ proj/resources/views/welcome.blade.php
@@ -1,30 +1,91 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main-layout')
+@section('contenuto-pagina')
 
-        <title>Laravel</title>
+<section>
+<div class="jumbotron text-center mt-2" id="imgjumbotron">
+    <div class="container">
+    <div class="row">
+            <div class="col-12">
+        <h1 class="text-center margini">WELCOME TO BOOLIVEROO</h1>
+            <p><a class="btn btn-primary btn-lg" href="{{route('clientSearch')}}" role="button">Scopri i nostri ristoranti</a></p>
+        </div>
+    </div>
+    </div>
+</div>
+</section>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<section>
+  <div class="container">
+    <h2>La selezione di Booliveroo</h2>
+    <div class="row text-center">
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 pb-1 pb-md-0">
+        <div class="card">
+          <img class="card-img-top" src="" alt="Not found image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Find</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}"></script>
+</section>
 
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+@endsection
 
-            .full-height {
-                height: 100vh;
-            }
 
             .flex-center {
                 align-items: center;
