d6a280823a31dd44abe4ec2d07b0d66ee9903455
ChristianPucci91 <pucci.christian91@gmail.com>
2021-03-04 12:37:31 +0100

    modifiche pagine dish-index dish-show

--- proj/resources/views/pages/dish-index.blade.php
+++ proj/resources/views/pages/dish-index.blade.php
@@ -2,11 +2,29 @@
 
 @section('contenuto-pagina')
 
-  @foreach ($user -> dishes as $dish)
-  <li>{{$dish -> name}}
-    <a href="{{route('dish-edit', $dish->id)}}"><i class="fas fa-pencil-alt"></i></a>
-    <a href="{{route('dish-delete', $dish->id)}}"><i class="fas fa-trash"></i></a>
-  </li>
+
+  <div class="list-group">
+    @foreach ($user -> dishes as $dish)
+      <div href="#" class="list-group-item list-group-item-action flex-column  align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">  {{$dish -> name}} </h5>
+           <span>
+
+             <a class="m-2 display-6" href="{{route('dish-edit', $dish->id)}}"><i class="fas fa-pencil-alt"></i></a>
+             <a class="m-2" href="{{route('dish-delete', $dish->id)}}"><i class="fas fa-trash"></i></a>
+
+           </span>
+        </div>
+        <p class="mb-1">{{$dish -> ingredients}}.</p>
+        <p class="mb-1">{{$dish -> price}}.</p>
+        <p class="mb-1">{{$dish -> visible}}</p>
+      </div>
+  </div>
+
+
+
+
+
   @endforeach
 
   <a href="{{route('dish-create')}}">Crea un nuovo piatto</a>
