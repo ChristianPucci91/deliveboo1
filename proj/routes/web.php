808567cebaeee8531d8c32ddfd9eec9203f4c070
Alessiogarfo92 <alessiogarfo92@gmail.com>
2021-03-05 18:45:47 +0100

    regist braintree e imp view e post funzionanti

--- proj/routes/web.php
+++ proj/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request; 
 
 /*
 |--------------------------------------------------------------------------
@@ -92,3 +93,57 @@ Route::post('/uploadImg','HomeController@updateImg')
 
 Route::get('/clearImg','HomeController@clearImg')
     ->name('clear-img');
+
+
+///////// ROTTA PAYMENT ///////////
+Route::get('/payment', function () {
+$gateway = new Braintree\Gateway([
+  'environment' => config('services.braintree.environment'),
+  'merchantId' => config('services.braintree.merchantId'),
+  'publicKey' => config('services.braintree.publicKey'),
+  'privateKey' => config('services.braintree.privateKey')
+]);
+
+$token = $gateway->ClientToken()->generate();
+
+  return view('test-payment', [
+    'token' => $token
+  ]);
+});
+
+Route::post('/checkout', function (Request $request){
+
+$gateway = new Braintree\Gateway([
+  'environment' => config('services.braintree.environment'),
+  'merchantId' => config('services.braintree.merchantId'),
+  'publicKey' => config('services.braintree.publicKey'),
+  'privateKey' => config('services.braintree.privateKey')
+]);
+// dd($request);
+$amount = $request->amount;
+$nonce = $request->payment_method_nonce;
+
+$result = $gateway->transaction()->sale([
+  'amount' => $amount,
+  'paymentMethodNonce' => $nonce,
+  'options' => [
+      'submitForSettlement' => true
+  ]
+]);
+
+if ($result->success) {
+    $transaction = $result->transaction;
+    // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+    return back()->with('success_message', 'Transaction succesful. The Id is :' . $transaction->id);
+} else {
+    $errorString = "";
+
+    foreach($result->errors->deepAll() as $error) {
+        $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+    }
+
+    // $_SESSION["errors"] = $errorString;
+    // header("Location: " . $baseUrl . "index.php");
+      return back()->withErrors('An error occured with the message: ' . $result->message);
+}
+});
